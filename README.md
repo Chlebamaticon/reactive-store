1ae81983ccd2f81cd3f8ba74a1967c6daa2aad0a
Dawid Wojda <dawiidio@gmail.com>
2018-11-16 01:22:52 +0100

    initial

---
+++ README.md
@@ -0,0 +1,85 @@
+# Reactive store
+
+Store for React which uses RxJS
+
+Project launch
+
+build
+```bash
+npm run build
+```
+
+### NPM install
+```bash
+npm i -P @dawiidio/reactive-store
+```
+
+### Simple case study:
+
+State creation:
+```jsx harmony
+// file: counterState.jsx
+import { map, merge, bufferTime } from "rxjs/operators";
+import { interval } from "rxjs";
+import { createState } from "../lib/store";
+
+export const counter = createState(
+  // first argument - initial state
+  {
+      counter: 0
+  },
+  // second - actions, all in state scope. Every action gets own, custom
+  // subject in first param and every action needs to return Observable.
+  // Real action exists in `map`, it gets value passed during call and 
+  // must return second function which will get in first param current state
+  // and return new one.
+  {
+    increment: (subject) => 
+      subject
+        .pipe(
+          merge(interval(1000)),
+          map((value) => (state) => ({
+                  ...state,
+                  counter: state.counter + value,
+              }
+            )
+          ),
+        )
+  }
+);
+``` 
+
+How to connect component and state:
+```jsx harmony
+// file: Counter.jsx
+import { counter } from 'counterState.jsx'; 
+
+export function _Counter({ counter, increment }) {
+  // increment(value) is action from our state and it is equal to
+  // call next(value)  on action subject
+  const onClick = () => increment(counter + 10);
+  
+  return (
+    <div>
+      <p>Counter value: </p>
+      <h3>{ counter }</h3>
+      <button onClick={onClick}>Add 10 to counter</button>
+    </div>
+  );
+}
+
+export const Counter = withStore(counter)(state => {
+  // state === state declared in store, you can pass many stores like 
+  // withStore(counter, users, ...etc) all their actions and fields will
+  // be available in connected component props. 
+  
+  // if you just return the same state without mutating like me then 
+  // leave call empty - withStore(counter)()(_Counter); which is equal 
+  // to state => state.
+  return state;
+}, actions => {
+  // second functions can mutate actions from stores, same as state, if you
+  // leave empty call it means "return all actions under the same names".
+  return actions;
+})(_Counter);
+```
---
+++ src/index.jsx
@@ -0,0 +1,127 @@
+/* eslint-disable */
+
+import React, { PureComponent } from 'react';
+import { Subject } from "rxjs";
+import { of } from "rxjs";
+import { publishReplay, refCount, merge, scan, map, skip } from "rxjs/operators";
+
+class Prevent extends PureComponent {
+  render() {
+    const { renderComponent, ...rest } = this.props;
+    return renderComponent(rest);
+  }
+}
+
+export function withStore(...localStates) {
+  return createStoreConnectComponent(createRootState(...localStates));
+}
+
+export function createStoreConnectComponent({ actions: rxActions, state$ }) {
+  const actions = Object.entries(rxActions)
+    .reduce((acc, [key, value]) => ({
+      ...acc,
+      [key]: value
+    }), {});
+
+  const defaultStateSelector = state => state;
+  const defaultActionsSelector = actions => actions;
+
+  return (selector = defaultStateSelector, actionsSelector = defaultActionsSelector) => (WrappedComponent) => {
+    const renderComponent = props => <WrappedComponent {...props} />;
+
+    class WithStore extends PureComponent {
+      static displayName = `Connect(${WrappedComponent.displayName || WrappedComponent.name || 'Unknown'})`;
+
+      componentDidMount() {
+        this.subscription = state$.pipe(map(selector)).subscribe(this.setState.bind(this));
+        this.actions = actionsSelector(actions);
+      }
+
+      componentWillUnmount() {
+        this.subscription.unsubscribe();
+      }
+
+      render() {
+        return (
+          <Prevent
+            renderComponent={renderComponent}
+            {...this.state}
+            {...this.actions}
+            {...this.props}
+          />
+        )
+      }
+    }
+
+    return WithStore;
+  };
+}
+
+export function createState(initialState, actionsFactories) {
+  const actionsArr = Object
+    .entries(actionsFactories)
+    .map(([key, action]) => {
+      const subjectForAction = new Subject();
+      const actionObservable = action(subjectForAction);
+
+      return { key, actionObservable };
+    });
+
+  const actions = actionsArr.reduce((acc, val) => {
+    const { key, actionObservable } = val;
+
+    return {
+      ...acc,
+      [key]: (value) => {
+        return actionObservable.next(value);
+      }
+    }
+  }, {});
+
+  const state$ = of(initialState).pipe(
+    merge(...actionsArr.map(({ actionObservable }) => actionObservable)),
+    scan((state, reducerFn) => reducerFn(state)),
+    publishReplay(1),
+    refCount(),
+  );
+
+  return {
+    state$,
+    actions,
+    initialState
+  }
+}
+
+export function createRootState(...localStates) {
+  let localStateObservers = [];
+  let rootInitialState = {};
+  let rootActions = {};
+
+  localStates.forEach(({ state$: internalState, initialState, actions }) => {
+    localStateObservers.push(internalState.pipe(
+      skip(1) //skip initial values for every state
+    ));
+
+    rootInitialState = {
+      ...rootInitialState,
+      ...initialState
+    };
+
+    rootActions = {
+      ...rootActions,
+      ...actions
+    };
+  });
+
+  const state$ = of(rootInitialState).pipe(
+    merge(...localStateObservers),
+    scan((oldState, newState) => ({ ...oldState, ...newState })),
+    publishReplay(1),
+    refCount()
+  );
+
+  return {
+    actions: rootActions,
+    state$
+  }
+}
